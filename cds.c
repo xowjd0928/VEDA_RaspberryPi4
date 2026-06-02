30bc7e9ac818ac60aace8b5bcda9e54a21fbc697
taejeong <taejeongkim98@gmail.com>
2026-06-02 12:41:43 +0900

    cds function added

---
+++ cds.c
@@ -0,0 +1,36 @@
+#pragma once
+#include "cds.h"
+
+void cds_function(int* threshold) {
+    FILE* log = fopen("cds.log", "a");
+    int fd;
+    int a2dChannel = 0;
+    int prev, a2dVal;
+
+    pthread_cleanup_push((void*)fclose, log);
+    fprintf(log, "[CDS Control] Current Threshold Value: %d\n", a2dVal);
+    fflush(log);
+
+    if((fd = wiringPiI2CSetup(0x48)) < 0) {
+        printf("wiringPiI2CSetup failed\n");
+    }
+
+    int cnt = 0;
+    while(1) {
+        time_t now = time(NULL);
+        struct tm* t = localtime(&now);
+        wiringPiI2CWrite(fd, 0x00 | a2dChannel);
+        prev = wiringPiI2CRead(fd);
+        a2dVal = wiringPiI2CRead(fd);
+        if (a2dVal < *threshold) {
+            fprintf(log, "[%02d:%02d:%02d] Current CDS Value: %d (Bright)\n", t->tm_hour, t->tm_min, t->tm_sec, a2dVal);
+            fflush(log);
+        } else {
+            fprintf(log, "[%02d:%02d:%02d] Current CDS Value: %d (Dark)\n", t->tm_hour, t->tm_min, t->tm_sec, a2dVal);
+            fflush(log);
+        }
+        delay(1000);
+        cnt++;
+    }
+    pthread_cleanup_pop(1);
+}
---
+++ cds.h
@@ -0,0 +1,11 @@
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+
+#define SW 5
+#define CDS 0
+#define LED 1
+
+void cds_function(int* threshold);
