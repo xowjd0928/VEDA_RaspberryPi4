800c24fb3021751eb6da970c168e43cafd434610
taejeong <taejeongkim98@gmail.com>
2026-06-02 13:50:09 +0900

    webserver function declaration moved to header

--- webserver.c
+++ webserver.c
@@ -1,11 +1,5 @@
 #include "webserver.h"
 
-/* 스레드 처리를 위한 함수 */
-static void *clnt_connection(void *arg);
-int sendData(FILE* fp, char *ct, char *filename);
-void sendOk(FILE* fp);
-void sendError(FILE* fp);
-
 void* cds_function(void* arg) {
     void* handle;
     void (*fptr)(int*);
