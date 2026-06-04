bc4c93111194bc4bcde6af7d427d2513ad67af17
taejeong <taejeongkim98@gmail.com>
2026-06-04 13:55:45 +0900

    segment with comment added

--- segment.c
+++ segment.c
@@ -1,7 +1,9 @@
 #include "segment.h"
 
+// 7세그멘트 핀 번호 배열
 int pins[7] = {A, B, C, D, E, F, G};
 
+// 숫자별 7세그멘트 출력 정보 배열
 int digits[10][7] = {
     {0,0,0,0,0,0,1}, // 0
     {1,0,0,1,1,1,1}, // 1
@@ -15,6 +17,14 @@ int digits[10][7] = {
     {0,0,0,0,1,0,0}  // 9
 };
 
+/**
+ * @brief 스레드 종료 시 세그멘트 상태를 초기화한다.
+ *
+ * 모든 세그멘트를 OFF 상태로 변경하고
+ * 동적 할당된 메모리를 해제한다.
+ *
+ * @param arg 해제할 포인터 변수
+ */
 void cleanup_function(void* arg) {
     for (int i = 0; i < 7; i++) {
         digitalWrite(pins[i], HIGH);
@@ -22,10 +32,22 @@ void cleanup_function(void* arg) {
     free(arg);
 }
 
+/**
+ * @brief 카운트다운 타이머를 실행한다.
+ *
+ * 전달받은 숫자부터 0까지 1초 간격으로 감소시키며
+ * 7세그멘트에 표시한다.
+ *
+ * 카운트다운이 완료되면 부저음을 1초간 출력한다.
+ *
+ * @param arg 카운트다운 시작 번호
+ */
 void segment_function(int* arg) {
     pthread_cleanup_push(cleanup_function, arg);
+
     int num = *arg;
 
+    // 카운트다운 
     for (int i = num; i >= 0; i--) {
         pthread_testcancel();
         for (int j = 0; j < 7; j++) {
@@ -34,8 +56,10 @@ void segment_function(int* arg) {
         delay(1000);
     }
     
+    // 카운트 다운 완료 후 부저음 출력
     softToneWrite(SPKR, 2000);
     delay(1000);
     softToneWrite(SPKR, 0);
+
     pthread_cleanup_pop(1);
 }
--- segment.h
+++ segment.h
@@ -4,14 +4,40 @@
 #include <pthread.h>
 #include <stdlib.h>
 
+// 7세그멘트 A GPIO 핀 번호
 #define A 21
+// 7세그멘트 B GPIO 핀 번호
 #define B 22
+// 7세그멘트 C GPIO 핀 번호
 #define C 23
+// 7세그멘트 D GPIO 핀 번호
 #define D 24
+// 7세그멘트 E GPIO 핀 번호
 #define E 25
+// 7세그멘트 F GPIO 핀 번호
 #define F 26
+// 7세그멘트 G GPIO 핀 번호
 #define G 27
+
+// 부저 핀 번호
 #define SPKR  6
 
+/**
+ * @brief 카운트다운 타이머를 실행하는 함수
+ *
+ * 전달받은 숫자부터 0까지 1초 간격으로 카운트다운을 수행한다.
+ * 카운트다운이 종료되면 부저를 울린다.
+ *
+ * @param arg 카운트다운 시작 번호
+ */
 void segment_function(int* arg);
+
+/**
+ * @brief 세그멘트 함수 종료 시 자원을 정리하는 함수
+ *
+ * 모든 세그먼트를 OFF 상태로 변경하고
+ * 동적 할당된 메모리를 해제한다.
+ *
+ * @param arg 스레드 생성 인자
+ */
 void cleanup_function(void* arg);
